91bf6dfb2fd955fe7515e306c6e2aa593003aee3
Anthony <anthonyguerrero62@outlook.com>
2023-06-05 10:05:57 -0700

    heroku

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const path = require('path');
+const path = require('path');
 // const routes = require('./routes');
 const {ApolloServer} = require('apollo-server-express');
 
